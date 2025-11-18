da7cc4777b2f3b1dc24d9746657a7fccc927ed2e
sudipeb <sudip.koirala@ebpearls.com>
2025-11-18 10:06:05 +0545

    autorouter and hive config created

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:hive_ce_flutter/hive_flutter.dart';
+import 'package:sangam/features/auth/data/datasource/auth_remote_data_source.dart';
+import 'package:sangam/features/auth/data/repository/auth_repo_impl.dart';
+import 'package:sangam/features/auth/domain/usecase/login_user.dart';
+import 'package:sangam/features/auth/domain/usecase/register_user.dart';
+import 'package:sangam/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:sangam/router/app_router.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('authBox');
+  final authRepository = AuthRepositoryImpl(
+    remoteDataSource: AuthRemoteDataSource(),
+  );
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => AuthBloc(
+            loginUser: LoginUser(authRepository),
+            registerUser: RegisterUser(authRepository),
+          ),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,6 +36,9 @@ class MyApp extends StatelessWidget {
   final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(routerConfig: _appRouter.config());
+    return MaterialApp.router(
+      routerConfig: _appRouter.config(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }

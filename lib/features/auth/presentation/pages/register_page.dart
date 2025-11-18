791e144fa3c6ef69dfb6651482e96aa4698f0287
sudipeb <sudip.koirala@ebpearls.com>
2025-11-18 10:12:03 +0545

    user registration page created

--- lib/features/auth/presentation/pages/register_page.dart
+++ lib/features/auth/presentation/pages/register_page.dart
@@ -1,12 +1,73 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart' show BlocConsumer, ReadContext;
+import 'package:sangam/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:sangam/features/auth/presentation/bloc/auth_event.dart';
+import 'package:sangam/features/auth/presentation/bloc/auth_state.dart';
+import 'package:sangam/router/app_router.gr.dart';
 
 @RoutePage()
 class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
-
+  RegisterPage({super.key});
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(context) {
-    return Scaffold();
+    return Scaffold(
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthSuccess) {
+            context.router.push(const LoginRoute());
+          } else if (state is AuthFailure) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.message)));
+            debugPrint(state.message);
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return Padding(
+            padding: EdgeInsetsGeometry.all(20),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                    labelText: "Enter Your Full Name",
+                  ),
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(labelText: "Enter Email"),
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: "Enter Your Password",
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<AuthBloc>().add(
+                      RegisterRequested(
+                        _nameController.text.trim(),
+                        _emailController.text.trim(),
+                        _passwordController.text.trim(),
+                      ),
+                    );
+                  },
+                  child: Text("Sign Up"),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }

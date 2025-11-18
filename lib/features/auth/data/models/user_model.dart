8413d525a2b14fd8d7b72ec0f1a2fe749c156ed0
sudipeb <sudip.koirala@ebpearls.com>
2025-11-18 14:56:24 +0545

    minor changes

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -3,6 +3,9 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'user_model.freezed.dart';
 part 'user_model.g.dart';
 
+///[UserModel]representsdata structure of a user coming from the backend
+///
+///extends the [User] entity
 @freezed
 abstract class UserModel with _$UserModel implements User {
   const factory UserModel({
@@ -11,6 +14,8 @@ abstract class UserModel with _$UserModel implements User {
     required String email,
     String? password,
   }) = _UserModel;
+
+  /// Factory constructor to parse a JSON map into a [UserModel] object.
   factory UserModel.fromJson(Map<String, dynamic> json) =>
       _$UserModelFromJson(json);
 }

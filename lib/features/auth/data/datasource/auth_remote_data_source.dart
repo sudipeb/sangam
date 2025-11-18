7203a242801e4df683d53931dc13515b20f3de5f
sudipeb <sudip.koirala@ebpearls.com>
2025-11-18 14:47:40 +0545

    endpoints sent as a constant

--- lib/features/auth/data/datasource/auth_remote_data_source.dart
+++ lib/features/auth/data/datasource/auth_remote_data_source.dart
@@ -1,25 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:sangam/core/constants/api_constants.dart';
 import 'package:sangam/core/network/api_client.dart';
 import 'package:sangam/features/auth/data/models/user_model.dart';
+import 'package:sangam/features/auth/domain/entity/user.dart';
 
 class AuthRemoteDataSource {
+  /// Instance of [ApiClient] to make network requests
   final ApiClient apiClient = ApiClient();
-  Future<UserModel> login(String email, String password) async {
+
+  /// Secure storage to save access and refresh tokens
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  /// Login: returns User after saving tokens
+  Future<User> login(String email, String password) async {
+    //  Login and get tokens
     final response = await apiClient.post(
-      "api/v1/auth/login",
+      ApiEndpoints.login,
       data: {"email": email, "password": password},
     );
-    if (response.data['user'] == null) {
-      throw Exception("User field missing in response");
+
+    final accessToken = response.data['accessToken'];
+    final refreshToken = response.data['refreshToken'];
+
+    if (accessToken == null || refreshToken == null) {
+      throw Exception("Login failed: Tokens missing");
     }
 
-    return UserModel.fromJson(response.data['user']);
+    //Save tokens securely
+    await _storage.write(key: 'accesstoken', value: accessToken);
+    await _storage.write(key: 'refreshtoken', value: refreshToken);
+
+    debugPrint("Tokens saved successfully!");
+
+    //Fetch the logged-in user
+    final userResponse = await apiClient.get("api/v1/auth/profile");
+
+    if (userResponse.data['user'] == null) {
+      throw Exception("User data missing after login");
+    }
+
+    //Return the User object
+    return UserModel.fromJson(userResponse.data['user']);
   }
 
-  Future<UserModel> register(String name, String email, String password) async {
+  ///Returns a [User] object if successfully registered
+  Future<User> register(String name, String email, String password) async {
     final response = await apiClient.post(
-      "api/v1/auth/register",
+      ApiEndpoints.register,
       data: {"name": name, "email": email, "password": password},
     );
+
     if (response.data['user'] == null) {
       throw Exception("User field missing in response");
     }

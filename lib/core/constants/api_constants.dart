154361140e683f1a0b3c6c179fcd782e62c25021
sudipeb <sudip.koirala@ebpearls.com>
2025-11-18 14:47:02 +0545

    endpoint constants set

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,3 +2,10 @@ class ApiConstants {
   static const String baseUrl =
       'https://social-media-backend-2nw5.onrender.com/';
 }
+
+class ApiEndpoints {
+  static const String register = "api/v1/auth/register";
+  static const String login = "api/v1/auth/login";
+  static const String profile = "api/v1/auth/profile";
+  static const String refreshToken = "api/v1/auth/refresh-token";
+}

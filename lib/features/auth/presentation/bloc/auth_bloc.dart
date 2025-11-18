34baaa63d472028e605b12feee6fc41a420f80bc
sudipeb <sudip.koirala@ebpearls.com>
2025-11-18 14:35:25 +0545

    user passed as argument and better exception handling approach

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,23 +1,31 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sangam/core/network/network_exceptions.dart';
 import 'package:sangam/features/auth/domain/usecase/login_user.dart';
 import 'package:sangam/features/auth/domain/usecase/register_user.dart';
 import 'package:sangam/features/auth/presentation/bloc/auth_event.dart';
 import 'package:sangam/features/auth/presentation/bloc/auth_state.dart';
 
+///[Bloc] to handle authentication logic
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final LoginUser loginUser;
   final RegisterUser registerUser;
   AuthBloc({required this.loginUser, required this.registerUser})
     : super(AuthInitial()) {
+    // Handler for LoginRequested event
     on<LoginRequested>((event, emit) async {
       emit(AuthLoading());
       try {
         final user = await loginUser.execute(event.email, event.password);
-        emit(AuthSuccess(user));
+        emit(AuthSuccess(user: user));
       } catch (e) {
-        emit(AuthFailure(e.toString()));
+        if (e is NetworkExceptions) {
+          emit(AuthFailure(message: e.message));
+        } else {
+          emit(AuthFailure(message: e.toString()));
+        }
       }
     });
+    // Handler for RegisterRequested event
     on<RegisterRequested>((event, emit) async {
       emit(AuthLoading());
       try {
@@ -26,9 +34,13 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
           event.email,
           event.password,
         );
-        emit(AuthSuccess(user));
+        emit(AuthSuccess(user: user));
       } catch (e) {
-        emit(AuthFailure(e.toString()));
+        if (e is NetworkExceptions) {
+          emit(AuthFailure(message: e.message));
+        } else {
+          emit(AuthFailure(message: e.toString()));
+        }
       }
     });
   }

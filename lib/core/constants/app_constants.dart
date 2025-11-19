a7d910cef36a9a728aa79e6ccd854bdb9ce1c262
sudipeb <sudip.koirala@ebpearls.com>
2025-11-19 14:54:01 +0545

    app constants added

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,7 +1,11 @@
 class ImageConstant {
   static const String _gifPath = "assets/gifs";
+  static const String _imagePath = "assets/images";
 
   //splashScreen
   static const String splashImage = "$_gifPath/splash.gif";
   static const String loginImage = "$_gifPath/edited.gif";
+
+  //user sign up
+  static const String userRegistration = "$_imagePath/userRegistration.webp";
 }

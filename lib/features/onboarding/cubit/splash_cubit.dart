f357f6cf6ff9650b2c19ddefe07c574952a405a6
sudipeb <sudip.koirala@ebpearls.com>
2025-11-19 11:08:10 +0545

    splash cubit logic changed

--- lib/features/onboarding/cubit/splash_cubit.dart
+++ lib/features/onboarding/cubit/splash_cubit.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:sangam/features/onboarding/cubit/splash_state.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -7,24 +7,21 @@ class SplashCubit extends Cubit<SplashState> {
   SplashCubit() : super(SplashInitial());
 
   Future<void> startSplash() async {
-    final storage = FlutterSecureStorage();
     await Future.delayed(Duration(seconds: 2));
 
     final prefs = await SharedPreferences.getInstance();
     final isFirstLaunch = prefs.getBool("first_launch") ?? true;
+    final isLoggedIn = prefs.getBool("is_logged_in") ?? false;
 
     if (isFirstLaunch) {
-      prefs.setBool("first_launch", false);
+      await prefs.setBool("first_launch", false);
       emit(SplashNavigateToRegistration());
-      return;
-    }
-
-    final token = await storage.read(key: "access_token");
-
-    if (token != null) {
+    } else if (isLoggedIn) {
       emit(SplashNavigateToHome());
     } else {
       emit(SplashNavigateToRegistration());
     }
+    debugPrint("isFirstTIME:$isFirstLaunch");
+    debugPrint("isLoggedIn:$isLoggedIn");
   }
 }

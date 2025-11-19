6f66617a97f7e46e5b0715149839acb2b3f59925
sudipeb <sudip.koirala@ebpearls.com>
2025-11-19 11:06:07 +0545

    home landing page created

---
+++ lib/features/home/presentation/pages/home_landing_page.dart
@@ -0,0 +1,19 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class HomeLandingPage extends StatelessWidget {
+  const HomeLandingPage({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Sangam",
+          style: Theme.of(context).appBarTheme.titleTextStyle,
+        ),
+      ),
+    );
+  }
+}

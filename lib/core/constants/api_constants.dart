893da1e8c3c0fe4ffca886afd97b5e18b8bc6d3e
sudipeb <sudip.koirala@ebpearls.com>
2025-12-04 17:39:25 +0545

    added the endpoints for the api endpoints

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -4,10 +4,17 @@ class ApiConstants {
 }
 
 class ApiEndpoints {
+  ///Authentication Endpoints
   static const String register = "api/v1/auth/register";
   static const String login = "api/v1/auth/login";
   static const String profile = "api/v1/auth/profile";
   static const String refreshToken = "api/v1/auth/refresh-token";
   static const String forgotPassword = "api/v1/auth/forgot-password";
   static const String resetPassword = "api/v1/auth/reset-password";
+
+  ///Post Endpoints
+  static const String createPost = "api/v1/post/create";
+  static const String postFeeds = "api/v1/post/feeds";
+  static const String editPost = "api/v1/post/edit";
+  static const String allPost = "api/v1/post/all";
 }
